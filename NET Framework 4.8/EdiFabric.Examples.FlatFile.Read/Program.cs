824f1bcdf0acc2fcd77eb5b7e8301093c648dbc1
Kamen Nikolov <kamen.nikolov@edifabric.com>
2023-07-21 13:10:34 +0100

    Updated to the latest version of EdiFabric

--- NET Framework 4.8/EdiFabric.Examples.FlatFile.Read/Program.cs
+++ NET Framework 4.8/EdiFabric.Examples.FlatFile.Read/Program.cs
@@ -6,7 +6,7 @@ namespace EdiFabric.Examples.FlatFile.Read
     {
         static void Main(string[] args)
         {
-            SerialKey.Set(Common.SerialKey.Get());
+            TokenFileCache.Set();
 
             ReadCSVFile.Run();
             ReadCSVFile.Run2();
--- NET Framework 4.8/EdiFabric.Examples.FlatFile.Write/Program.cs
+++ NET Framework 4.8/EdiFabric.Examples.FlatFile.Write/Program.cs
@@ -6,7 +6,7 @@ namespace EdiFabric.Examples.FlatFile.Write
     {
         static void Main(string[] args)
         {
-            SerialKey.Set(Common.SerialKey.Get());
+            TokenFileCache.Set();
             WriteCSVFile.Run();
             WriteCSVFileAsync.Run();
         }
