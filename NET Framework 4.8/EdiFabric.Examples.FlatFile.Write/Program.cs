720f57b5fb813fac075a22d5b5d4ed884302fdf0
Kamen Nikolov <kamen.nikolov@edifabric.com>
2023-09-25 14:15:01 +0100

    Updated to EdiFabric 10.6.7

--- NET Framework 4.8/EdiFabric.Examples.FlatFile.Write/Program.cs
+++ NET Framework 4.8/EdiFabric.Examples.FlatFile.Write/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EdiFabric.Examples.FlatFile.Common;
+using System;
 
 namespace EdiFabric.Examples.FlatFile.Write
 {
@@ -6,7 +7,17 @@ namespace EdiFabric.Examples.FlatFile.Write
     {
         static void Main(string[] args)
         {
-            SerialKey.Set(Config.TrialSerialKey);
+            try
+            {
+                SerialKey.Set(Config.TrialSerialKey, true);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.StartsWith("Can't set token"))
+                    throw new Exception("Your trial has expired! To continue using EdiFabric SDK you must purchase a plan from https://www.edifabric.com/pricing.html");
+            }
+            //  Uncomment and then comment out the line above if you wish to use distributed cache for tokens
+            //  TokenFileCache.Set();
             WriteCSVFile.Run();
             WriteCSVFileAsync.Run();
         }
